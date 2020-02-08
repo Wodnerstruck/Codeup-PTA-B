dc97a3fe7a8da2e74015c8acc98f2ee056bc3e33
Xuewei Xiong <xiongxuewei1996@outlook.com>
2020-02-08 21:21:26 +0800

    Hash

--- 字符串hash.cpp
+++ 字符串hash.cpp
@@ -4,7 +4,7 @@ int hashFunc_26(char S[],int len){//将字符串S转换为整数
     for(int i=0;i<len;i++){
         id=id*26+(S[i]-'A');//26进制转换为10进制
     }
-    return 0;
+    return id;
 }
 //如包含大写字母和小写字母，采用52进制
 int hashFunc_52(char S[],int len){
@@ -17,5 +17,5 @@ int hashFunc_52(char S[],int len){
             id=id*52+(S[i]-'a')+26;
         }
     }
-    return 0;
+    return id;
 }
